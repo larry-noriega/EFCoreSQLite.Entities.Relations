bf1574e6e9554fbdd473ef2cec1ff28231391535
larry-noriega <larry.noriega@hotmail.com>
2023-05-20 16:42:23 -0500

    feat: - [x] Classroom entity feat: - [x] Course entity feat: - [x] Student entity feat: - [x] Teacher entity

---
+++ EFCollections/Course.cs
@@ -0,0 +1,14 @@
+﻿namespace EFCollections
+{
+	public class Course
+	{
+		public int? Id { get; set; }
+		public string? Title { get; set; }
+
+		public ICollection<Student>? Students { get; set; }
+
+		public Teacher? Author { get; set; }
+		public Teacher? Editor { get; set; }
+
+	}
+}
---
+++ EFCollections/Student.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EFCollections
+{
+	public class Student
+	{
+		public int? Id { get; set; }
+		public string? Name { get; set; }
+
+		[Required]
+		public Classroom? Classroom { get; set; }
+
+		public ICollection<Course>? Courses { get; set; }
+	}
+}
